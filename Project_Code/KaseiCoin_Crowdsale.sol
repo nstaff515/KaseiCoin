a89ff3d08e504884310afbcb79cd3d0a44ec0be0
Nelson <nstaff515@gmail.com>
2022-08-31 12:37:32 -0400

    added contracts

---
+++ Project_Code/KaseiCoin_Crowdsale.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.5.0;
+
+import "./KaseiCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+
+
+// Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
+// * Crowdsale
+// * MintedCrowdsale
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+    
+    // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
+    constructor(
+        uint rate,
+        address payable wallet,
+        KaseiCoin token
+    ) 
+     Crowdsale(rate, wallet, token) 
+     public 
+    
+    {
+         
+    }
+}
+
+contract KaseiCoinCrowdsaleDeployer {
+
+    address public kasei_token_address;
+    address public kasei_crowdsale_address;
+
+    constructor(
+       string memory name,
+       string memory symbol,
+       address payable wallet //this address will receive all of the ETH from the crowdsale
+    )
+        public
+    {
+        // Create the KaseiCoin token
+        KaseiCoin token = new KaseiCoin(name, symbol, 0);
+        kasei_token_address = address(token);
+
+        // Create the KaseiCoinCrowdsale and tell it about KaseiCoin
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale(1, wallet, token);
+        kasei_crowdsale_address = address(kasei_crowdsale);
+
+        // Set the `KaseiCoinCrowdsale` contract as a minter and have KaseiCoinCrowdsaleDeployer renounce its minter role.
+        token.addMinter(kasei_crowdsale_address);
+        token.renounceMinter();
+    }
+}
